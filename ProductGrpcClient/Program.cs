60c5d42e9df360f9ab9a199a762da234a0fe1957
Thilakshan Baskaran <telakshanb@gmail.com>
2023-03-03 14:07:11 -0500

    add workerservice template

--- ProductGrpcClient/Program.cs
+++ ProductGrpcClient/Program.cs
@@ -11,17 +11,25 @@ class Program
     {
         Console.WriteLine("Greetings gRPC");
 
-        Thread.Sleep(2000);
+        //Thread.Sleep(2000);
 
         using var channel = GrpcChannel.ForAddress("https://localhost:7212");
 
         var client = new ProductProtoService.ProductProtoServiceClient(channel);
 
-/*        await GetProductAsync(client);
+        await GetProductAsync(client);
 
-        await GetAllProducts(client);*/
+        //await AddProductAsync(client);
 
-        await AddProduct(client);
+        await UpdateProductAsync(client);
+
+        await GetAllProducts(client);
+
+        await InsertBulkProductAsync(client);
+
+        await DeleteProductAsync(client);
+
+        Console.ReadLine();
 
         //using (var clientData = client.GetAllProducts(new GetAllProductsRequest()))
         //{
@@ -45,13 +53,11 @@ class Program
             Console.WriteLine(responseData);
         }
 
-        Console.ReadLine();
-
     }
 
     private static async Task GetProductAsync(ProductProtoService.ProductProtoServiceClient client)
     {
-        Console.WriteLine("GetProductAsync started...");
+        Console.WriteLine("\nGetProductAsync started...");
 
         var response = await client.GetProductAsync(new GetProductRequest
         {
@@ -62,29 +68,85 @@ class Program
     }
 
 
-    private static async Task AddProduct(ProductProtoService.ProductProtoServiceClient client)
+    private static async Task AddProductAsync(ProductProtoService.ProductProtoServiceClient client)
     {
-        Console.WriteLine("Add Product started...");
+        Console.WriteLine("\nAdd Product started...");
 
-/*        var response = await client.AddProductAsync(
+        var response = await client.AddProductAsync(
             new AddProductRequest
             {
                 Product = new ProductModel
                 {
-                    Name = "Red",
-                    Description = "New Red Phone Mi10T",
+                    Name = "Note 23",
+                    Description = "New Samsung Note",
                     Price = 699,
                     Status = ProductStatus.Instock,
                     CreatedTime = Timestamp.FromDateTime(DateTime.UtcNow)
                 }
             });
 
-        Console.WriteLine("ProductModel: " + response.ToString());*/
-
-        //await GetAllProducts(client);
+        Console.WriteLine("ProductModel: " + response.ToString());
 
     }
 
+    private static async Task DeleteProductAsync(ProductProtoService.ProductProtoServiceClient client)
+    {
+        Console.WriteLine("\nDelete Product started...");
 
+        var response = await client.DeleteProductAsync(
+            new DeleteProductRequest
+            {
+                ProductId = 5
+            });
+
+        Console.WriteLine("DeleteProductResponse: " + response.ToString());
+
+    }
+
+    private static async Task UpdateProductAsync(ProductProtoService.ProductProtoServiceClient client)
+    {
+        Console.WriteLine("\nUpdateProductAsync Product started...");
+
+        var response = await client.UpdateProductAsync(
+            new UpdateProductRequest
+            {
+                Product = new ProductModel
+                {
+                    ProductId = 3,
+                    Name = "Samsung S23+",
+                    Description = "New Samsung phone",
+                    Price = 699,
+                    Status = ProductStatus.Instock,
+                    CreatedTime = Timestamp.FromDateTime(DateTime.UtcNow)
+                }
+            });
+
+        Console.WriteLine("UpdateProductResponse: " + response.ToString());
+    }
+
+    private static async Task InsertBulkProductAsync(ProductProtoService.ProductProtoServiceClient client)
+    {
+        Console.WriteLine("\nInsertBulkProductAsync Product started...");
+
+        using var clientBulk = client.InsertBulkProduct();
+
+        for (int i = 0; i < 3; i++)
+        {
+            var productModel = new ProductModel
+            {
+                Name = $"Product{i}",
+                Description = "Bulk inserted product",
+                Price = 399,
+                Status = ProductStatus.Instock,
+                CreatedTime = Timestamp.FromDateTime(DateTime.UtcNow)
+            };
+
+            await clientBulk.RequestStream.WriteAsync(productModel);
+        }
+
+        await clientBulk.RequestStream.CompleteAsync();
+        var responseBulk = await clientBulk;
+        Console.WriteLine($"Status: {responseBulk.Success}. Insert Count: {responseBulk.InsertCount}");
+    }
 
 }
